88f3707a5be73bc066c7094dc4e1a98d59e92c02
Leva <mariownyou@gmail.com>
2021-02-28 12:48:58 +0300

    добавил путь для получения id папки

--- api.py
+++ api.py
@@ -1,11 +1,21 @@
 from flask import Flask
-
+from app import DriveApi
 
 app = Flask(__name__)
+drive = DriveApi()
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    files = drive.get_all('projects')
+    print(files)
+    return files[0]
+
+@app.route('/api/<folder>')
+def folder(folder):
+    files = drive.get_all(folder)
+    if len(files) == 0:
+        return '<h1>Empty folder</h1>'
+    return f'<pre>{files}</pre>'
 
 if __name__ == '__main__':
     app.run()
